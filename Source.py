9c73dafbbf053f3c4458039cc3e1f549b08be778
axelroitman <38115641+axelroitman@users.noreply.github.com>
2021-03-13 19:20:40 -0300

    Avances sobre Euler (todavía no funcional)

--- Source.py
+++ Source.py
@@ -11,18 +11,33 @@ import sympy
 """
 
 def euler(y0,x0,xf,function,N):
-    rang = list(range(x0,xf + 1))
     
     y = y0
     x = x0
     h = (xf - x0) / N
+
+    ##rang = list(range(x0,xf + 1))
+    rang = np.arange (x0, xf, h)
+    
     
     fResult = {}
-    
+    0
     for k in rang:        
-        functionEvaluated = 1234 #functionEvaluated = LÓGICA DE SYMPY. Provisorio lo setee con un valor para que no tire error
+        #functionEvaluated = 1234 #functionEvaluated = LÓGICA DE SYMPY. Provisorio lo setee con un valor para que no tire error
+        expr = sympy.sympify(function)
+        f = sympy.lambdify((x,y),expr)
+        functionEvaluated = f(x,y)
         y = y + h * functionEvaluated
         fResult[x] = y
         x = x0 + k * h
 
+    print(fResult) 
 
+x0 = float(input('x0= '))
+y0 = float(input('y0= '))
+xf = float(input('xf= '))
+function = input('function= ')
+N = float(input('N= '))
+
+#euler(0.32,0.13,0.14,"sin(x)-ln(y)",4)
+euler(y0,x0,xf,function,N)
